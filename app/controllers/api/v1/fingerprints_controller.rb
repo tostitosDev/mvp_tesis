b88668c8292fdadceb1aab0abaeecab805250f40
Tostitos <lestat__x@hotmail.com>
2018-04-17 03:57:50 -0300

    test add fingerprint init ok

--- app/controllers/api/v1/fingerprints_controller.rb
+++ app/controllers/api/v1/fingerprints_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::FingerprintsController < ApplicationController
   def create
     Fingerprint.transaction do
       @employee = Employee.find_by(rut: params[:rut])
-      @fingerprint = Fingerprint.create hash_fingerprint: params[:hash_fingerprint], employee_id: @employee_id.id, id_in_sensor: params[:id_in_sensor]
+      @fingerprint = Fingerprint.create hash_fingerprint: params[:hash_fingerprint], employee_id: @employee.id, id_in_sensor: params[:id_in_sensor]
       render :json => {:result => 'ok'}
     end
   rescue ActiveRecord::RecordInvalid => exception
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: "json" } do
     namespace :v1 do
       resources :marks
+      resources :fingerprints
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
