f11f583b0e81bacc1b63313b004ae94701d17ff6
Tostitos <lestat__x@hotmail.com>
2018-04-19 03:01:09 -0300

    dashboard count according aasm_state

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,11 @@
 class DashboardsController < ApplicationController
   def index
+    
+    @correct  = Mark.select(:employee_id).distinct.where(type_mark: 1, aasm_state: 'correct', date_time_mark: Date.today.at_beginning_of_day..Date.today.at_end_of_day)
+    puts 'correct'
+    puts @correct.count
+    @delayed  = Mark.select(:employee_id).distinct.where(type_mark: 1, aasm_state: 'delayed')
+    @out      = Mark.select(:employee_id).distinct.where(type_mark: 1, aasm_state: 'out')
+    @away     = Mark.select(:employee_id).distinct.where(type_mark: 1, aasm_state: 'away')
   end
 end
