7df9a25c897036f76b5030d622633a1623fe698b
Tostitos <lestat__x@hotmail.com>
2018-04-21 14:23:44 -0300

    filtro ok

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -1,7 +1,7 @@
 class MarksController < ApplicationController
   def index
     if params[:start_date] != nil || params[:end_date] != nil
-      puts 'jelou'
+      @marks = Mark.daily_marks(params[:start_date].to_datetime.beginning_of_day, params[:end_date].to_datetime.end_of_day)
     else
       @start = @start_date = Date.today.beginning_of_day
       @marks = Mark.daily_marks(@start)
--- app/models/mark.rb
+++ app/models/mark.rb
@@ -10,6 +10,7 @@ class Mark < ApplicationRecord
   scope :by_day,    ->(employee, day) {where(employee_id: employee.id, date_time_mark: day.beginning_of_day..day.end_of_day)}
 
   scope :daily_marks, ->(day) {where(date_time_mark: day.beginning_of_day..day.end_of_day)}
+  scope :daily_marks, ->(start_day, end_day) {where(date_time_mark: start_day..end_day)}
   aasm do 
     #Marcacion correcta, atrasada, fuera de lugar o ausente.
     state :correct, initial: true
